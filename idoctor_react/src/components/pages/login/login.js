b533bd3ba943ea59dcd399a0c68f75f0adb5a4e8
AlexThunder <thunderson28@gmail.com>
2018-05-19 18:56:33 +0300

    init

--- idoctor_react/src/components/pages/login/login.js
+++ idoctor_react/src/components/pages/login/login.js
@@ -5,15 +5,44 @@ import Button from 'react-materialize/lib/Button'
 import './login.css'
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('email - ', this.state.email);
+        console.log('pass - ', this.state.email);
+    };
+    handleChange = (e) => {
+        let name = e.target.name;
+        this.setState({
+            [name]:e.target.value
+        })
+    };
     render() {
+        console.log(this.state);
         return (
             <div className='login'>
                 <Row>
-                    <Input type="email" label="Емеіл" s={12} />
-                    <Input type="password" label="Пароль" s={12} />
-                    <Button waves='light'>Увійти</Button>
+                    <Input
+                        type="email"
+                        label="Емеіл" s={12}
+                        name='email'
+                        value={this.state.email}
+                        onChange={this.handleChange}/>
+                    <Input
+                        type="password"
+                        label="Пароль"
+                        s={12}
+                        name='password'
+                        value={this.state.password}
+                        onChange={this.handleChange}/>
 
-
+                    <Button waves='light' onClick={this.handleSubmit}>Увійти</Button>
                 </Row>
             </div>
         );
