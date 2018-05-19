e602fc7264a880134c8425cd1572d65d6801e546
AlexThunder <thunderson28@gmail.com>
2018-05-19 12:24:19 +0300

    public

--- idoctor_react/src/App.js
+++ idoctor_react/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Simptom from './components/pages/simptoms/simptoms'
+import Pharm from './components/pages/pharm/pharm'
 import 'materialize-css/dist/css/materialize.min.css'
 import './App.css';
 
@@ -7,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Simptom />
+        <Pharm />
       </div>
     );
   }
