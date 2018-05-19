b0581b034052fdc1ad43c311aa3563bae3f2d4aa
AlexThunder <thunderson28@gmail.com>
2018-05-19 13:00:20 +0300

    routing

--- idoctor_react/src/App.js
+++ idoctor_react/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {Switch, Route} from 'react-router-dom'
 import Simptom from './components/pages/simptoms/simptoms'
 import Pharm from './components/pages/pharm/pharm'
 import 'materialize-css/dist/css/materialize.min.css'
@@ -8,7 +9,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Pharm />
+        <Switch>
+          <Route exact path="/simptom" component={Simptom}/>
+          <Route exact path="/pharm" component={Pharm}/>
+        </Switch>
       </div>
     );
   }
