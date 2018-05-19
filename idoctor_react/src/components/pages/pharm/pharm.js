4bb12a72ed307282f6257b989b771dc27a014040
AlexThunder <thunderson28@gmail.com>
2018-05-19 12:47:26 +0300

    public add

--- idoctor_react/src/components/pages/pharm/pharm.js
+++ idoctor_react/src/components/pages/pharm/pharm.js
@@ -1,31 +1,29 @@
 import React from  'react'
+import Collection from 'react-materialize/lib/Collection'
+import CollectionItem from 'react-materialize/lib/CollectionItem'
 import Card from 'react-materialize/lib/Card'
+import './pharm.css'
 
 class Pharm extends React.Component {
     render () {
         return (
             <section className="Pharm">
                 <div className="container">
-                    <Card className='blue-grey darken-1'
-                          textClassName='white-text'
+                    <Card textClassName='black-text'
                           title='Запис до лікаря'
-                          actions={[<a href='#'>This is a link</a>]}>
+                          actions={[<a href='#'>Записатись на прийом</a>]}>
                         I am a very simple card.
                     </Card>
-                    <Card className='blue-grey darken-1'
-                          textClassName='white-text'
+                    <Card textClassName='black-text'
                           title='Ймовірна хвороба'>
                         I am a very simple card.
                     </Card>
-                    <Card className='blue-grey darken-1'
-                          textClassName='white-text'
-                          title='Ліки, які допоможуть'>
-                        <ul>
-                            <li>Ліки </li>
-                            <li></li>
-                            <li></li>
-                        </ul>
-                    </Card>
+                    <Collection header='Ліки, які допоможуть'>
+                        <CollectionItem>Alvin1</CollectionItem>
+                        <CollectionItem>Alvin2</CollectionItem>
+                        <CollectionItem>Alvin3</CollectionItem>
+                        <CollectionItem>Alvin4</CollectionItem>
+                    </Collection>
                 </div>
             </section>
         )
